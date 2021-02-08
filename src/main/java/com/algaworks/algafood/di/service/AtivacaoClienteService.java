96fec3bbae7ec10b94dbbb12cf2b754c0a2a02ca
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-08 08:16:59 -0300

    Desambiguação de beans com anotação Qualifier

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.di.service;
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,10 +12,9 @@ public class AtivacaoClienteService {
     private final Notificador notificador;
 
     @Autowired
-    public AtivacaoClienteService(Notificador notificador) {
+    public AtivacaoClienteService(@Qualifier("urgente") Notificador notificador) {
         this.notificador = notificador;
     }
-
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
