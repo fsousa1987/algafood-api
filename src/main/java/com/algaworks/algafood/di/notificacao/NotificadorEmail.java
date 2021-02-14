eab1fd5fa504ed90c28946e02204dc33f430c896
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-14 07:26:53 -0300

    Configurando projetos Spring Boot com o application.properties

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,12 +1,10 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import static com.algaworks.algafood.di.notificacao.NivelUrgencia.*;
+import static com.algaworks.algafood.di.notificacao.NivelUrgencia.SEM_URGENCIA;
 
-@Profile("prod")
 @TipoDoNotificador(SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
