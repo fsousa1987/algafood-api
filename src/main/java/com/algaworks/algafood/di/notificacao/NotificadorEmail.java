14ec4800fc3e6bc944645efd9fa3d7a6efc20e5c
fsousa1987 <franciscoeds1987@gmail.com>
2021-02-17 12:33:49 -0300

    Acessando propriedades com anotação ConfigurationProperties

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static com.algaworks.algafood.di.notificacao.NivelUrgencia.SEM_URGENCIA;
@@ -10,16 +10,17 @@ import static com.algaworks.algafood.di.notificacao.NivelUrgencia.SEM_URGENCIA;
 @Component
 public class NotificadorEmail implements Notificador {
 
-    @Value("${notificador.email.host-servidor}")
-    private String host;
+    private final NotificadorProperties properties;
 
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
+    @Autowired
+    public NotificadorEmail(NotificadorProperties properties) {
+        this.properties = properties;
+    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.println("Host: " + host);
-        System.out.println("Porta: " + porta);
+        System.out.println("Host: " + properties.getHostServidor());
+        System.out.println("Porta: " + properties.getPortaServidor());
 
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
