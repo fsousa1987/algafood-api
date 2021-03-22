a3a6d3c3941c39839ce7c8d91c84bd0d7e158002
fsousa1987 <franciscoeds1987@gmail.com>
2021-03-22 15:31:06 -0300

    Consultando Singleton Resource com GET e a anotação PathVariable

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,15 +3,15 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cozinhas") //, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
     private final CozinhaRepository cozinhaRepository;
@@ -21,15 +21,14 @@ public class CozinhaController {
         this.cozinhaRepository = cozinhaRepository;
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Cozinha> listar1() {
+    @GetMapping
+    public List<Cozinha> listar() {
         System.out.println("LISTAR 1");
         return cozinhaRepository.listar();
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public List<Cozinha> listar2() {
-        System.out.println("LISTAR 2");
-        return cozinhaRepository.listar();
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.buscar(cozinhaId);
     }
 }
