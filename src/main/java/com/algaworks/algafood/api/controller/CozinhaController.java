ec66439ad305ba370032738da99395779fa8b026
fsousa1987 <franciscoeds1987@gmail.com>
2021-03-27 07:33:07 -0300

    Implementando a camada de domain services (e a importância da linguagem ubíqua)

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -18,10 +19,12 @@ import java.util.List;
 public class CozinhaController {
 
     private final CozinhaRepository cozinhaRepository;
+    private final CadastroCozinhaService cadastroCozinha;
 
     @Autowired
-    public CozinhaController(CozinhaRepository cozinhaRepository) {
+    public CozinhaController(CozinhaRepository cozinhaRepository, CadastroCozinhaService cadastroCozinha) {
         this.cozinhaRepository = cozinhaRepository;
+        this.cadastroCozinha = cadastroCozinha;
     }
 
     @GetMapping
@@ -48,7 +51,7 @@ public class CozinhaController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-        return cozinhaRepository.salvar(cozinha);
+        return cadastroCozinha.salvar(cozinha);
     }
 
     @PutMapping("/{cozinhaId}")
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCozinhaService {
+
+    private final CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    public CadastroCozinhaService(CozinhaRepository cozinhaRepository) {
+        this.cozinhaRepository = cozinhaRepository;
+    }
+
+    public Cozinha salvar(Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
+    }
+}
