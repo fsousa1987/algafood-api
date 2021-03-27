7e55eb541535f1c1db5659512264466d61d309a1
fsousa1987 <franciscoeds1987@gmail.com>
2021-03-27 14:12:37 -0300

    Implementando serviços REST de cidades e estados

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
