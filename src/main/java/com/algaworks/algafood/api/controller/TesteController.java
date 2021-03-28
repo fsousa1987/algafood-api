b58d7e288a57281f377c17e92b1ee9d37b6ef093
fsousa1987 <franciscoeds1987@gmail.com>
2021-03-28 18:10:26 -0300

    Criando consultas com query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,22 +1,33 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SuppressWarnings("CommentedOutCode")
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
 
-//    private final CozinhaRepository cozinhaRepository;
-//
-//    @Autowired
-//    public TesteController(CozinhaRepository cozinhaRepository) {
-//        this.cozinhaRepository = cozinhaRepository;
-//    }
-//
-//    @GetMapping("/cozinhas/por-nome")
-//    public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//        return cozinhaRepository.consultarPorNome(nome);
-//    }
+    private final CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    public TesteController(CozinhaRepository cozinhaRepository) {
+        this.cozinhaRepository = cozinhaRepository;
+    }
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome) {
+        return cozinhaRepository.findTodasByNome(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
 }
