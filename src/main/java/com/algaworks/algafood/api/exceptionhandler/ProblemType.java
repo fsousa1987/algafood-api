f1ade53eb3eefe70a8458b0163ce006bc9da3b09
fsousa1987 <franciscoeds1987@gmail.com>
2021-04-15 07:28:18 -0300

    Usando o formato de problemas no corpo de respostas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-    ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontrada");
+    ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontrada"),
+    ENTIDADE_EM_USO("Entidade em uso", "/entidade-em-uso"),
+    ERRO_NEGOCIO("Violação de regra de negócio", "/erro-negocio");
 
     private final String uri;
     private final String title;
