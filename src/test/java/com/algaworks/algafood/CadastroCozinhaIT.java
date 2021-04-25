ea01382affec6f8afba1854ac97b1c44ddb00dd4
fsousa1987 <franciscoeds1987@gmail.com>
2021-04-25 07:34:05 -0300

    Criando um método para fazer setup dos testes

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,13 +21,16 @@ public class CadastroCozinhaIT {
    @LocalServerPort
    private int port;
 
+   @Before
+   public void setUp() {
+      RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+      RestAssured.port = port;
+      RestAssured.basePath = "/cozinhas";
+   }
+
    @Test
    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-      RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
       given()
-              .basePath("/cozinhas")
-              .port(port)
               .accept(ContentType.JSON)
       .when()
               .get()
@@ -36,11 +40,7 @@ public class CadastroCozinhaIT {
 
    @Test
    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
-      RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
       given()
-              .basePath("/cozinhas")
-              .port(port)
               .accept(ContentType.JSON)
       .when()
               .get()
