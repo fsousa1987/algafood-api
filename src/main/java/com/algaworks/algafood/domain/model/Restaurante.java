1a8d44b144badccc8b7205a2392c0203e2fdb64d
fsousa1987 <franciscoeds1987@gmail.com>
2021-05-07 13:59:43 -0300

    Implementando endpoints de associação de usuários responsáveis com restaurantes

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -54,6 +54,12 @@ public class Restaurante {
         inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private Set<FormaPagamento> formasPagamento = new HashSet<>();
 
+    @ManyToMany
+    @JoinTable(name = "restaurante_usuario_responsavel",
+            joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "usuario_id"))
+    private Set<Usuario> responsaveis = new HashSet<>();
+
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
 
@@ -79,5 +85,13 @@ public class Restaurante {
 
     public void fechar() {
         setAberto(false);
+    }
+
+    public void removerResponsavel(Usuario usuario) {
+        getResponsaveis().remove(usuario);
+    }
+
+    public void adicionarResponsavel(Usuario usuario) {
+        getResponsaveis().add(usuario);
     }
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -1,10 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
-import com.algaworks.algafood.domain.model.Cidade;
-import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.model.FormaPagamento;
-import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.model.*;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,15 +14,18 @@ public class CadastroRestauranteService {
     private final CadastroCozinhaService cadastroCozinha;
     private final CadastroCidadeService cadastroCidade;
     private final CadastroFormaPagamentoService cadastroFormaPagamento;
+    private final CadastroUsuarioService cadastroUsuario;
 
     @Autowired
     public CadastroRestauranteService(RestauranteRepository restauranteRepository,
                                       CadastroCozinhaService cadastroCozinha, CadastroCidadeService cadastroCidade,
-                                      CadastroFormaPagamentoService cadastroFormaPagamento) {
+                                      CadastroFormaPagamentoService cadastroFormaPagamento,
+                                      CadastroUsuarioService cadastroUsuario) {
         this.restauranteRepository = restauranteRepository;
         this.cadastroCozinha = cadastroCozinha;
         this.cadastroCidade = cadastroCidade;
         this.cadastroFormaPagamento = cadastroFormaPagamento;
+        this.cadastroUsuario = cadastroUsuario;
     }
 
     @Transactional
@@ -78,6 +78,22 @@ public class CadastroRestauranteService {
     public void fechar(Long restauranteId) {
         Restaurante restauranteAtual = buscarOuFalhar(restauranteId);
         restauranteAtual.fechar();
+    }
+
+    @Transactional
+    public void desassociarResponsavel(Long restauranteId, Long usuarioId) {
+        Restaurante restaurante = buscarOuFalhar(restauranteId);
+        Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
+
+        restaurante.removerResponsavel(usuario);
+    }
+
+    @Transactional
+    public void associarResponsavel(Long restauranteId, Long usuarioId) {
+        Restaurante restaurante = buscarOuFalhar(restauranteId);
+        Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
+
+        restaurante.adicionarResponsavel(usuario);
     }
 
     public Restaurante buscarOuFalhar(Long restauranteId) {
