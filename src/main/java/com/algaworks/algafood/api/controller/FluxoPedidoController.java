9ca3121623f12fb9c9a3404dfa8a50fb165c9db4
fsousa1987 <franciscoeds1987@gmail.com>
2021-05-09 10:11:15 -0300

    Implementando endpoint de transição de status de pedidos

---
+++ src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.service.FluxoPedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/pedidos/{pedidoId}")
+public class FluxoPedidoController {
+
+    private final FluxoPedidoService fluxoPedido;
+
+    @Autowired
+    public FluxoPedidoController(FluxoPedidoService fluxoPedido) {
+        this.fluxoPedido = fluxoPedido;
+    }
+
+    @PutMapping("/confirmacao")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void confirmar(@PathVariable Long pedidoId) {
+        fluxoPedido.confirmar(pedidoId);
+    }
+}
