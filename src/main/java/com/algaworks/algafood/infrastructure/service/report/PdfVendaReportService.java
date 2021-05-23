2472361b646620834d32daa04012fe13f6978dd4
fsousa1987 <franciscoeds1987@gmail.com>
2021-05-23 15:46:52 -0300

    Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/algaworks/algafood/infrastructure/service/report/PdfVendaReportService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/report/PdfVendaReportService.java
@@ -1,14 +1,47 @@
 package com.algaworks.algafood.infrastructure.service.report;
 
-import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
-import com.algaworks.algafood.domain.service.VendaReportService;
+import java.util.HashMap;
+import java.util.Locale;
+
+import net.sf.jasperreports.engine.JasperPrint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class PdfVendaReportService implements VendaReportService {
 
+    private final VendaQueryService vendaQueryService;
+
+    @Autowired
+    public PdfVendaReportService(VendaQueryService vendaQueryService) {
+        this.vendaQueryService = vendaQueryService;
+    }
+
     @Override
     public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
-        return null;
+        try {
+            var inputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+
+            var parametros = new HashMap<String, Object>();
+            parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+            var vendasDiarias = vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
+            var dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros, dataSource);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        }
+        catch (Exception e) {
+            throw new ReportException("Não foi possível emitir relatório de vendas diárias", e);
+        }
     }
 }
---
+++ src/main/java/com/algaworks/algafood/infrastructure/service/report/ReportException.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.infrastructure.service.report;
+
+import java.io.Serial;
+
+public class ReportException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ReportException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ReportException(String message) {
+        super(message);
+    }
+}
