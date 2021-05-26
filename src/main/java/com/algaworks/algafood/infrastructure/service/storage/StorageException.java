4d11e828f78a1a68eeaf159ab931e89cac02a6ac
fsousa1987 <franciscoeds1987@gmail.com>
2021-05-26 16:02:10 -0300

    Implementando o serviço de armazenagem de fotos no disco local

---
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/StorageException.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.infrastructure.service.storage;
+
+import java.io.Serial;
+
+public class StorageException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
