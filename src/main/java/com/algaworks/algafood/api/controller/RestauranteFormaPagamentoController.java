88975f66e416d19d06205428ad98faea090b2598
Francisco Sousa <franciscoeds1987@gmail.com>
2021-06-28 08:50:10 -0300

    Descrevendo documentação de endpoints de associação de restaurantes com formas de pagamento

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -2,17 +2,19 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
+import com.algaworks.algafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/restaurantes/{restauranteId}/formas-pagamento")
-public class RestauranteFormaPagamentoController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
     private final CadastroRestauranteService cadastroRestaurante;
     private final FormaPagamentoModelAssembler formaPagamentoModelAssembler;
