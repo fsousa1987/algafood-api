3d0d33252d7b73722ffccf9409676f35ffee10aa
Francisco Sousa <franciscoeds1987@gmail.com>
2021-06-29 08:15:19 -0300

    Descrevendo documentação de endpoints de produtos

--- src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -13,9 +13,8 @@ public interface RestauranteUsuarioResponsavelControllerOpenApi {
     @ApiResponses({
             @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
     })
-    List<UsuarioModel> listar(
-            @ApiParam(value = "ID do restaurante", example = "1", required = true)
-                    Long restauranteId);
+    List<UsuarioModel> listar(@ApiParam(value = "ID do restaurante", example = "1", required = true)
+                                      Long restauranteId);
 
     @ApiOperation("Desassociação de restaurante com usuário responsável")
     @ApiResponses({
@@ -23,23 +22,14 @@ public interface RestauranteUsuarioResponsavelControllerOpenApi {
             @ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado",
                     response = Problem.class)
     })
-    void desassociar(
-            @ApiParam(value = "ID do restaurante", example = "1", required = true)
-                    Long restauranteId,
-
-            @ApiParam(value = "ID do usuário", example = "1", required = true)
-                    Long usuarioId);
+    void desassociar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
+                     @ApiParam(value = "ID do usuário", example = "1", required = true) Long usuarioId);
 
     @ApiOperation("Associação de restaurante com usuário responsável")
     @ApiResponses({
             @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
-            @ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado",
-                    response = Problem.class)
+            @ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", response = Problem.class)
     })
-    void associar(
-            @ApiParam(value = "ID do restaurante", example = "1", required = true)
-                    Long restauranteId,
-
-            @ApiParam(value = "ID do usuário", example = "1", required = true)
-                    Long usuarioId);
+    void associar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
+                  @ApiParam(value = "ID do usuário", example = "1", required = true) Long usuarioId);
 }
